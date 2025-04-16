67033ff3f568544f562b516c1a7740ec97ee40d1
thiagolj <jesus.alves@aluno.ifsp.edu.br>
2025-04-16 20:28:30 -0300

    Add files via upload

---
+++ teste 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int base;
+	int aresta;
+	int altura;
+	int area;
+	
+	cout << "Informe a base da aresta ";
+	cin >> base;
+	cout << "Infome a altura da aresta ";
+	cin >> altura;
+	
+	area = aresta * aresta;
+	
+	cout << "A area do quadrado e " << area << endl;
+	
+	return 0;
+}
