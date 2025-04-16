f5554c4991f25223d376253fc5755fa465ef99e2
thiagolj <jesus.alves@aluno.ifsp.edu.br>
2025-04-16 20:17:51 -0300

    Add files via upload

---
+++ NovoArquivo.cpp
@@ -0,0 +1,25 @@
+#include  <iostream>
+using namespace std;
+
+int main()
+{
+	int base;
+	int altura;
+	int area;
+	
+	cout << "Informe a base do retangulo ";
+	cin >> base;
+	cout << "Informe a altura do retangulo ";
+	cin >> altura;
+	
+	area = base * altura;
+	
+	cout << "A area do retanfulo e " << area << endl;
+	
+	return 0;
+
+
+
+	
+	
+}
