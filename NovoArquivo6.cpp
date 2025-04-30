3c3a1e9f4a7e7d32134fb07a46b9ddd95632fa1a
thiagolj <jesus.alves@aluno.ifsp.edu.br>
2025-04-30 20:29:21 -0300

    Add files via upload

---
+++ NovoArquivo6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double diametro;
+	double area;
+	double raio;
+	cout << " informe o diametro ";
+	cin >> diametro;
+	
+   raio = diametro/2;
+   
+   area = 3,14 * raio;
+   	cout << " a area do circulo e " << area << endl;
+	
+}
